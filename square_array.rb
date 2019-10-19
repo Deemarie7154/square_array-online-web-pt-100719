d44ed500a7940356ef73420bd4f7d68d653186d2
Dwight Marie <dwightmarie@gmail.com>
2019-10-19 18:46:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,4 @@
 def square_array(numbers)
-  n_a = [] #new code declaring an empty array to store the results.
-numbers.each do|num|
-puts num ** 2
-n_a << num**2   #new code to push results into an array
-end
+ n_a = [] #new code declaring an empty array to store the results.
+numbers.each{ |num| n_a << num ** 2}   #new code to push results into an array
 end
